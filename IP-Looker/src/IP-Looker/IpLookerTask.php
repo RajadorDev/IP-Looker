24900ac3b9264509b645ffdd90a60d1d2a299931
RajadorDev <125204182+RajadorDev@users.noreply.github.com>
2023-08-11 14:40:35 -0300

    update IP-Looker/src/IP-Looker/IpLookerTask.php

---
+++ IP-Looker/src/IP-Looker/IpLookerTask.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace IP_Looker;
+
+use pocketmine\Server;
+
+use pocketmine\plugin\PluginBase;
+
+class IpLookerTask extends PluginBase 
+{
+	
+	public function onRun()
+	{
+		
+	}
+	
+	public function onComplemetion(Server $s)
+	{
+		
+	}
+	
+}
