c1fb72a0671bea404c294b4c2d77ebee0a1df316
RajadorDev <125204182+RajadorDev@users.noreply.github.com>
2023-08-11 15:22:33 -0300

    update IP-Looker/src/IP-Looker/IpLookerCommand.php

---
+++ IP-Looker/src/IP-Looker/IpLookerCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace IP-Looker;
+
+use pocketmine\command\{CommandSender, Command};
+
+use IPLooker;
+
+class IpLookerCommand extends Command 
+{
+	
+	protected $system = null;
+	
+	public function __construct(IPLooker $system)
+	{
+		$this->system = $system;
+	}
+	
+	public function execute(CommandSender $p, String $label, array $args)
+	{
+		if($this->testPermission($p))
+		{
+			$prefix = '§8-==(§eIPLooker§8)==-';
+			if($this->system->haveServerIp())
+			{
+				$ip = $this->system->getServerIp();
+				$p->sendMessage(
+					$prefix.PHP_EOL.
+					'§8> §eServer Address: §f' . $ip
+					);
+			}
+		}
+	}
+	
+}
