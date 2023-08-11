2a01c0f6cdffddd75634021956a25984d71c73eb
RajadorDev <125204182+RajadorDev@users.noreply.github.com>
2023-08-11 15:59:47 -0300

    update IP-Looker/src/IP-Looker/IPLooker.php

--- IP-Looker/src/IP-Looker/IPLooker.php
+++ IP-Looker/src/IP-Looker/IPLooker.php
@@ -17,6 +17,16 @@ class IPLooker extends PluginBase
 	
 	public function onEnable()
 	{
+			$this->getLogger()->info
+			(
+			'        §eIP-Looker        ' . PHP_EOL . 
+			'§8> §fCreated by: §eRajador Developer' . PHP_EOL . 
+			'§8> §9Discord: §erajadortv' . PHP_EOL .
+			'§8> §fInstagram: §e@rajadortv' . PHP_EOL . 
+			'§8> §cYouTube: §ehttps://www.youtube.com/channel/UC1UJFxth-YRkNuLBqBYyqbA'. PHP_EOL .
+			'§8> §bGit§6Hub: §eRajadorDev'
+			);
+	
 		$this->getServer()->getCommandMap()->register('iplooker', new IpLookerCommand($this));
 		$this->startIpLooker();
 	}
@@ -49,16 +59,6 @@ class IPLooker extends PluginBase
 	
 	public function startIpLooker()
 	{
-		$this->getLogger()->info
-		(
-			'        §eIP-Looker        ' . PHP_EOL . 
-			'§8> §fCreated by: §eRajador Developer' . PHP_EOL . 
-			'§8> §9Discord: §erajadortv' . PHP_EOL .
-			'§8> §fInstagram: §e@rajadortv' . PHP_EOL . 
-			'§8> §cYouTube: §ehttps://www.youtube.com/channel/UC1UJFxth-YRkNuLBqBYyqbA'. PHP_EOL .
-			'§8> §bGit§6Hub: §eRajadorDev'
-			
-		);
 		$this->getServer()->getScheduler()->scheduleAsyncTask(new IpLookerTask);
 		$this->isLooking = true;
 		$this->getLogger()->notice('Looking for server address....');
