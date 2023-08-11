3a04b5c904a89e77e1062b5da1653b0a0aca30be
RajadorDev <125204182+RajadorDev@users.noreply.github.com>
2023-08-10 22:53:50 -0300

    update IP-Looker/src/IP-Looker/IP-Looker.php

--- IP-Looker/src/IP-Looker/IP-Looker.php
+++ IP-Looker/src/IP-Looker/IP-Looker.php
@@ -1,1 +1,43 @@
+<?php
 
+namespace IPLooker; //TODO fzr depois
+
+use pocketmine\plugin\PluginBase;
+
+class IPLooker extends PluginBase 
+{
+	
+	protected $ip = null;
+	
+	protected $isLooking = false;
+	
+	public function onEnable()
+	{
+		
+	}
+	
+	public function isLookingForIp() : bool 
+	{
+		return $this->isLooking;
+	}
+	
+	public function startIpLooker()
+	{
+		
+	}
+	
+	public function finishIpLooker(array $result)
+	{
+		if($result[0] == false)
+		{
+			$error = isset($result[1]) ? $result[1] : 'Unknow ERROR';
+			return $this->getLogger()->critical($error);
+		}
+		
+		$this->ip = $result[1];
+		$this->getLogger()->notice('§eIP found: §f' . $result[1]);
+	}
+	
+	
+	
+}
