488983f5d1075ad649cf48619386c6461166b27a
RajadorDev <125204182+RajadorDev@users.noreply.github.com>
2023-08-11 15:44:01 -0300

    update IP-Looker/src/IP-Looker/IpLookerTask.php

--- IP-Looker/src/IP-Looker/IpLookerTask.php
+++ IP-Looker/src/IP-Looker/IpLookerTask.php
@@ -13,7 +13,24 @@ class IpLookerTask extends PluginBase
 	
 	public function onRun()
 	{
-		
+		$ch = curl_init(GetIpTask::API);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ["User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0 PocketMine-MP"]);
+    curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+    curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+    curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
+    $ret = curl_exec($ch);
+    if(curl_errno($ch))
+    {
+    	$this->setResult([$ret, curl_error($ch)]);
+    }else{
+      $this->setResult($ret);
+    }
+    curl_close($ch);
 	}
 	
 	public function onCompletion(Server $s)
